8c9ef367bfb71ae00ba632bdd77a5980cf711bfa
wonijjang0915 <55573431+wonijjang0915@users.noreply.github.com>
2019-10-31 15:51:05 +0900

    3

--- main.c
+++ main.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define SIZE 5
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
 	int i;
-	int grade[5];
+	int a[5] ={1,2,3,4,5};
+	int b[5] ={1,2,3,4,5};
 	
-	grade[0]=10;
-	grade[1]=20;
-	grade[2]=30;
-	grade[3]=40;
-	grade[4]=50;
 	for(i=0;i<5;i++)
-	 printf("grade[%d] = %d\n", i, grade[i] );
+	{
+		if(a[i] |=b[i])
+		{
+			printf("%i is different\n",i);
+
+		}
+	}
+	
 	return 0;
 }
